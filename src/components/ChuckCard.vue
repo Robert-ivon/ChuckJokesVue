ed8634bb7a7880f9aa01fda1971c46d82d1e8680
Robert-ivon <al02803380@tecmilenio.mx>
2024-11-13 17:44:56 -0600

    Task10

---
+++ src/components/ChuckCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card" style="width: 18rem;">
+      <img :src="icon_url" class="card-img-top" alt="Chuck Norris">
+      <div class="card-body">
+        <p class="card-text">{{ value }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "ChuckCard",
+    props: {
+      icon_url: String,
+      value: String
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Add custom styling if needed */
+  </style>
+  
